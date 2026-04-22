e30106e0093e054e1c7384d65f192e90367ba1ab
bermet <bermet.alibek@gmail.com>
2026-04-21 21:59:07 -0500

    Update CoinAcceptor to implement PaymentAcceptor interface

--- src/model/CoinAcceptor.java
+++ src/model/CoinAcceptor.java
@@ -1,17 +1,25 @@
 package model;
 
-public class CoinAcceptor {
+public class CoinAcceptor implements PaymentAcceptor {
     private int amount;
 
     public CoinAcceptor(int amount) {
         this.amount = amount;
     }
 
+    @Override
     public int getAmount() {
         return amount;
     }
 
-    public void setAmount(int amount) {
-        this.amount = amount;
+    @Override
+    public void acceptPayment() {
+        amount += 10;
+        System.out.println("Вы пополнили баланс на 10");
+    }
+
+    @Override
+    public void deduct(int price) {
+        amount -= price;
     }
 }
